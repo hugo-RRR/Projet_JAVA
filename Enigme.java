4df4e9bfe3ed1382a762aab8cc1341c9930cd07f
hugo-RRR <103840388+hugo-RRR@users.noreply.github.com>
2022-04-17 15:23:49 +0200

    Add files via upload

---
+++ Enigme.java
@@ -0,0 +1,115 @@
+package Classes;
+
+import java.util.Scanner;
+
+public class Enigme {
+	
+	public float duree;
+	public String typeEnigme;
+	public String essai;
+	public String reponse;
+	public String ennonce [];
+	public int nbEssai;
+	
+	
+
+	public Enigme(float duree, String typeEnigme, String essai, String reponse, String[] ennonce, int nbEssai) {
+		super();
+		this.duree = duree;
+		this.typeEnigme = typeEnigme;
+		this.essai = essai;
+		this.reponse = reponse;
+		this.ennonce = ennonce;
+		this.nbEssai = nbEssai;
+	}
+
+
+
+	public float getDuree() {
+		return duree;
+	}
+
+
+	public void setDuree(float duree) {
+		this.duree = duree;
+	}
+
+
+	public String getTypeEnigme() {
+		return typeEnigme;
+	}
+
+
+
+	public void setTypeEnigme(String typeEnigme) {
+		this.typeEnigme = typeEnigme;
+	}
+
+
+	public String getEssai() {
+		return essai;
+	}
+
+
+	public void setEssai(String essai) {
+		this.essai = essai;
+	}
+
+
+
+	public String getReponse() {
+		return reponse;
+	}
+
+
+
+	public void setReponse(String reponse) {
+		this.reponse = reponse;
+	}
+
+
+
+	public String[] getEnnonce() {
+		return ennonce;
+	}
+
+
+
+	public void setEnnonce(String[] ennonce) {
+		this.ennonce = ennonce;
+	}
+
+
+
+	public int getNbEssai() {
+		return nbEssai;
+	}
+
+
+
+	public void setNbEssai(int nbEssai) {
+		this.nbEssai = nbEssai;
+	}
+
+
+
+	public static void repondre(String essai) {
+		
+		Scanner rep = new Scanner(System.in);
+		System.out.println("Quel est votre réponse ? ");
+		essai = rep.next();
+		if( essai == reponse) {
+			System.out.println("Bravo vous avez reussi l'énigme, vous gagnez donc un fragment de clé. /n Vous pouvez vous déplacer dans une nouvelle salle." );
+			
+		}
+		return;
+	}
+	
+	
+	
+	
+	
+
+}
+
+//  
