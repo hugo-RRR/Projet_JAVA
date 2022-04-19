439cc4e3d5042676f6652d34e3157d53dbbd9aed
William <will.i.amdu8313@gmail.com>
2022-04-19 22:36:33 +0200

    Commande sortie

--- Commande.java
+++ Commande.java
@@ -1,5 +1,30 @@
 package Classes;
 
-public class Commande {
+import java.util.Scanner;
 
+public class Commande {
+	
+	public static String reponse;
+	
+}
+
+public void DemandSortie() {
+	Scanner rep = new Scanner(System.in);
+    System.out.println("Choisissez une sortie : N = Nord, S = Sud, E = Est, O = Ouest");
+    reponse = rep.next();
+    rep.close();
+    if(reponse == "N")
+    	SortieNord();
+    else if(reponse == "S")
+    	SortieSud();
+    else if(reponse == "E")
+    	SortieEst();
+    else if(reponse == "O")
+    	SortieOuest();
+    else {
+    	System.out.println("Erreur de saisie, veuillez réessayer\n");
+    	DemandSortie();
+    }
+    			
+}
 }
