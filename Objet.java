74196e295d0a23c297d847491ed927823065801f
William <will.i.amdu8313@gmail.com>
2022-04-20 21:45:40 +0200

    Update Objet.java

--- Objet.java
+++ Objet.java
@@ -1,5 +1,20 @@
 package Classes;
 
 public class Objet {
+	
+	private String nom;
+	private int prix;
+	
+	public Objet(String name, int prix) {
+		this.nom = name;
+		this.prix = prix;
+	}
 
+	public String getNom() {
+		return this.nom;
+	}
+	
+	public int getPrix() {
+		return this.prix;
+	}
 }
